13f485c89ed8bbd9365b8b0541c39a6ffab20c83
ekakele <ekaakelenjeridze@gmail.com>
2023-12-10 22:26:03 +0400

    update main page to create an overview

--- Assignment-27/GroceryShopView.swift
+++ Assignment-27/GroceryShopView.swift
@@ -11,80 +11,150 @@ struct GroceryShop: View {
     //MARK: Properties
     
     var body: some View {
-        ZStack {
-            Color(red: 0.95, green: 0.96, blue: 0.97)
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            NavigationView {
+        NavigationView {
+            ZStack(alignment: .top) {
+                Color.white
+                    .edgesIgnoringSafeArea(.all)
                 
-                VStack(alignment: .leading) {
+                
+                VStack(spacing: 6) {
                     
                     Image("banner")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .cornerRadius(20)
-                        .padding(20)
                     
-                    ProductCell()
+                    //products
+                    VStack(alignment: .leading, spacing: 4) {
+                        //section title
+                        Text("Vegetables")
+                            .font(.title3)
+                            .bold()
+                            .foregroundColor(.black)
+                        
+                        CustomDivider()
+
+                        
+                        ProductCell()
+                        
+                        //section title
+                        Text("Meat")
+                            .font(.title3)                            .bold()
+                            .foregroundColor(.black)
+                        
+                        CustomDivider()
+
+                        
+                        ProductCell()
+                        
+                        //section title
+                        Text("Seafood")
+                            .font(.title3)                            .bold()
+                            .foregroundColor(.black)
+                        
+                        CustomDivider()
+                        
+                        ProductCell()
+                    }
                 }
-                .navigationBarTitle("Grocery Shop", displayMode: .inline)
+                .padding(.horizontal, 12)
+                .navigationBarTitle("Shop Grocery Online", displayMode: .inline)
             }
+            //            .padding(.vertical, 0)
+            
         }
     }
 }
 
 //MARK: - Product
 struct ProductCell: View {
-    //MARK: Properties
-    
     var body: some View {
         VStack {
             
             Image("broccoli")
                 .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 50, height: 50)
             
-            Spacer()
-                        
-            //info & add button stack
-            HStack(spacing: 25) {
-                //title & price stack
-                VStack(spacing: 8) {
-                    Text("Broccoli")
-                        .font(.system(size: 20))
-                        .bold()
-                        .foregroundColor(.black)
-                    
-                    Text("$2.99 (kg)")
-                        .font(.system(size: 16))
-                        .bold()
-                        .foregroundColor(.black)
+            //title & price stack
+            VStack(spacing: 2) {
+                Text("Broccoli")
+                    .font(.system(size: 16))
+                    .bold()
+                    .foregroundColor(.black)
+                
+                Text("$2.99 (kg)")
+                    .font(.system(size: 12))
+                    .foregroundColor(.black)
+            }.padding(.bottom, 3)
+            
+            //plus / minus buttons & product quantity
+            HStack(spacing: 12) {
+                Button(action: {
+                    print("pressed")
+                }, label: {
+                    Image(systemName: "minus.circle.fill")
+                        .resizable()
+                        .foregroundColor(Color(red: 0.14, green: 0.67, blue: 0.29))
+                        .frame(width: 20, height: 20)
                 }
-                                
+                )
+                
+                
+                //quantity & +/- buttons
+                ZStack{
+                    Color.white
+                        .frame(width: 20, height: 20)
+                        .cornerRadius(6)
+                    
+                    Text("1")
+                        .font(.system(size: 12))
+                        .foregroundColor(.black)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(Color.gray, lineWidth: 1.0)
+                                .frame(width: 20, height: 20)
+                                .opacity(0.5)
+                        )
+                }
+                
+                
                 Button(action: {
                     print("pressed")
                 }, label: {
                     Image(systemName: "plus.circle.fill")
                         .resizable()
                         .foregroundColor(Color(red: 0.14, green: 0.67, blue: 0.29))
-                        .frame(width: 36, height: 36)
+                        .frame(width: 20, height: 20)
                 }
-                        
                 )
             }
-
-            Spacer()
         }
-        .frame(width: 160, height: 180)
+        .frame(width: 110, height: 120)
+        .padding(.vertical, 14)
         .background(Color(red: 0.95, green: 0.96, blue: 0.97))
         .cornerRadius(16)
+        
         .overlay(
             RoundedRectangle(cornerRadius: 16)
                 .stroke(Color.gray, lineWidth: 1.0)
                 .opacity(0.5)
-    )
+        )
         
         
+    }
+}
+
+//MARK: - Custom Divider
+struct CustomDivider: View {
+    var body: some View {
+        VStack {
+            Divider()
+                .background(Color.gray)
+                .frame(height: 2)
+                .opacity(0.5)
+                .padding(.leading, 150)
+        }
     }
 }
 //MARK: - Preview
---
+++ Assignment-27/ProductModel.swift
@@ -0,0 +1,86 @@
+//
+//  ProductModel.swift
+//  Assignment-27
+//
+//  Created by Eka Kelenjeridze on 10.12.23.
+//
+
+import Foundation
+
+
+
+struct Product: Identifiable {
+    let id = UUID()
+    let image: String
+    let title: String
+    var price: String
+    //    var stockAmount: Int
+}
+
+
+struct ProductList {
+    static let Vegetables = [
+        Product(
+            image: "broccoli",
+            title: "Broccoli",
+            price: "3.99$ (kg)"
+        ),
+        Product(
+            image: "carrot",
+            title: "Carrot",
+            price: "0.99$ (kg)"
+        ),
+        Product(
+            image: "pepper",
+            title: "Bell Pepper",
+            price: "5.99$ (kg)"
+        ),
+        Product(
+            image: "ginger",
+            title: "Ginger",
+            price: "4.99$ (kg)"
+        ),
+        Product(
+            image: "lettuce",
+            title: "Lettuce",
+            price: "1.99$ (kg)"
+        ),
+        Product(
+            image: "squash",
+            title: "Butternut Squash",
+            price: "3.99$ (kg)"
+        )
+    ]
+    
+    static let Meat = [
+        Product(
+            image: "beef",
+            title: "Beef",
+            price: "18.99$ (kg)"
+        ),
+        Product(
+            image: "pork",
+            title: "Pork Steak",
+            price: "12.99$ (kg)"
+        ),
+        Product(
+            image: "chicken",
+            title: "Chicken Breast",
+            price: "9.99$ (kg)"
+        )
+    ]
+    
+    static let Seafood = [
+        Product(
+            image: "salmon",
+            title: "Salmon Fillet",
+            price: "39.99$ (kg)"
+        ),
+        Product(
+            image: "shrimps",
+            title: "Shrimps",
+            price: "34.99$ (kg)"
+        )
+    ]
+}
+
